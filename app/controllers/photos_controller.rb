c1c23aab09c4bb1018a1dc07f285dfa28e01e190
rschoder <rob.schoder@gmail.com>
2015-10-28 19:04:58 -0500

    Update 1

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,27 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({ :id => params[:id]})
+    @photo_source = @photo.source
+    @photo_caption = @photo.caption
+    @photo_id = @photo.id
+  end
 
+  def new_form
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+    redirect_to("localhost:3000/photos")
+  end
+
+  def delete_photo
+    @delete = Photo.find_by({ :id => params[:id]})
+    @delete.destroy
+    redirect_to("localhost:3000/photos")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
+  #Routes to CREATE photos
+  get("/photos/new", { :controller => "photos", :action => "new_form"})
+
+  get("/create_photo", { :controller => "photos", :action => "create_row"})
+
+  #routes to DELETE photos
+  get("/photos/delete_photo", { :controller => "photos", :action => "delete_photo"})
+
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
